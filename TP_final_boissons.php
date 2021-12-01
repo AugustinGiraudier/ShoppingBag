e8f49e78511dbf4285547ca5a69c276f84b94a73
ArthurSecher <arthursechercabot@gmail.com>
2021-12-01 15:26:59 +0100

    Premiere pages

---
+++ TP_final_boissons.php
@@ -0,0 +1,41 @@
+<!doctype html>
+<html lang="fr">
+<head>
+  <link rel="stylesheet" href="TP_final.css">
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+  <meta content= "text/html ; charset=utf-8" content="text/hmtl" />
+  <title> ISIWeb4Shop </title>
+</head>
+<body>
+  <p class="flotte">
+    <img src="saveur_imperial.jpg">
+  </p>
+  <br>
+  <h5>
+    Saveur Impériale 
+  </h5> <br>
+  <p id="descritpion">
+    Sachet de thé de qualité supérieure, 200 sachets par boite <br> <br>
+    <b> Notre prix : 4.99€ </b>
+  </p>
+
+  <br><br>
+  <p class="flotte">
+  <img src="jus_orange.jpg">
+  </p>
+
+
+  <h5>&nbsp;
+    Jus d'orange de floride
+  </h5> <br>
+  <p id="descritpion">&nbsp;
+    Bouteille d'un litre <br> <br>&nbsp;
+    <b> Notre prix : 2.95€ </b>
+  </p>
+
+
+  <br><br><br>
+  <div id="site"> 
+    L'ensemble de ce site est © ISIWeb4Shop 2021-2022.
+  </div>
+</body>
