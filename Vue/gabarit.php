1a36c631285da935f1ba52f5620c4d42c5698c15
augiraudie <augustin.giraudier@etu.uca.fr>
2021-12-08 16:30:36 +0100

    Ajout routeur

---
+++ Vue/gabarit.php
@@ -0,0 +1,17 @@
+<!doctype html>
+<html lang="fr">
+    <head>
+        <meta charset="UTF-8" />
+        <link rel="stylesheet" href="Contenu/style.css" />
+        <title><?= $titre ?></title>
+    </head>
+    <body>
+        <div id="global">
+            <h3>test</h3>
+            <div id="contenu">
+                <?= $contenu ?>
+            </div> <!-- #contenu -->
+            <h3>test</h3>
+        </div> <!-- #global -->
+    </body>
+</html>
---
+++ Vue/vueAccueil.php
@@ -0,0 +1,3 @@
+<?php $this->titre = "ISI WEB 4 Shop"; ?>
+
+<h1>uiiii</h1>
