2f245849866627350450b9009d4d97fff0a92013
augiraudie <augustin.giraudier@etu.uca.fr>
2022-01-11 18:38:26 +0100

    nouvelles infos database

--- Config/Constantes.php
+++ Config/Constantes.php
@@ -7,4 +7,10 @@ define("_BASE_URL", $BasUrl);
 define("_RESSOURCES_DIR",  "./Ressources");
 define("_PRODUCTS_IMAGES_DIR", _RESSOURCES_DIR . "/productimages");
 
+define("_DB_HOST", "localhost");
+define("_DB_NAME", "web4shop");
+define("_DB_USER", "root");
+define("_DB_PASSWORD", "");
+
+
 ?>
